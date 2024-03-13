8661901da95a1ffb69405480e770c8a693965dbd
josh-alhassan <saintjosh26@gmail.com>
2024-03-13 09:38:20 +0100

    add how async works

--- AsyncJsPromiseRequest/async.js
+++ AsyncJsPromiseRequest/async.js
@@ -1,3 +1,5 @@
+// Call Stack
+
 const multiply = (x, y) => x * y;
 
 const square = (x) => multiply(x, x);
@@ -7,3 +9,11 @@ const isRightTriangle = (a, b, c) => {
 };
 
 console.log(isRightTriangle(3, 4, 5));
+
+// How Asynchronous JavaScript Actually works
+console.log("I Happened First");
+setTimeout(() => {
+  console.log(" I happened 3rd");
+}, 3000);
+
+console.log("I happened 2nd");
