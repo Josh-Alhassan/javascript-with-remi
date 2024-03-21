85035481aa1bbb43c1b3d48cdcd59dddf7223a11
josh-alhassan <saintjosh26@gmail.com>
2024-03-21 12:10:23 +0100

    promise chaining

--- AsyncJsPromiseRequest/async.js
+++ AsyncJsPromiseRequest/async.js
@@ -73,7 +73,7 @@ function fakeRequest(url) {
       const pages = {
         "/users": [
           { id: 1, username: "Joshua" },
-          { id: 5, username: "Alhassan" },
+          { id: 5, username: "Abel" },
         ],
         "/about": "This is the about page",
       };
@@ -101,3 +101,81 @@ fakeRequest("/users")
     console.log(res.status);
     console.log("Request Failed");
   });
+
+//This is a FAKE Http Request Function
+//It takes 1 second to resolve or reject the promise, depending on the url that is passed in
+const fakeRequests = (url) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const pages = {
+        "/users": [
+          { id: 1, username: "Bilbo" },
+          { id: 5, username: "Esmerelda" },
+        ],
+        "/users/1": {
+          id: 1,
+          username: "Bilbo",
+          upvotes: 360,
+          city: "Lisbon",
+          topPostId: 454321,
+        },
+        "/users/5": {
+          id: 5,
+          username: "Esmerelda",
+          upvotes: 571,
+          city: "Honolulu",
+        },
+        "/posts/454321": {
+          id: 454321,
+          title: "Ladies & Gentlemen, may I introduce my pet pig, Hamlet",
+        },
+        "/about": "This is the about page!",
+      };
+      const data = pages[url];
+      if (data) {
+        resolve({ status: 200, data }); //resolve with a value!
+      } else {
+        reject({ status: 404 }); //reject with a value!
+      }
+    }, 1000);
+  });
+};
+
+fakeRequests("/users")
+  .then((res) => {
+    console.log(res);
+    const id = res.data[0].id;
+    return fakeRequests(`/users/${id}`);
+  })
+  .then((res) => {
+    console.log(res);
+    const postId = res.data.topPostId;
+    return fakeRequests(`/posts/${postId}`);
+  })
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log("OH NO!", err);
+  });
+
+// ************************************************
+// ATTEMPT 2 (deliberate error to illustrate CATCH)
+// ************************************************
+// fakeRequest('/users')
+// 	.then((res) => {
+// 		console.log(res);
+// 		const id = res.data[0].id;
+// 		return fakeRequest(`/useALSKDJrs/${id}`); //INVALID URL, CATCH WILL RUN!
+// 	})
+// 	.then((res) => {
+// 		console.log(res);
+// 		const postId = res.data.topPostId;
+// 		return fakeRequest(`/posts/${postId}`);
+// 	})
+// 	.then((res) => {
+// 		console.log(res);
+// 	})
+// 	.catch((err) => {
+// 		console.log('OH NO!', err);
+// 	});
