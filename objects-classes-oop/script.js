08ba549a76106aa75adce1682d24383b0b7d0176
josh-alhassan <saintjosh26@gmail.com>
2024-03-27 22:40:48 +0100

    accessor properties

--- objects-classes-oop/script.js
+++ objects-classes-oop/script.js
@@ -37,3 +37,26 @@ console.log(person2.name);
 person2.name = "Abel";
 delete person2.name;
 console.log(person2.name);
+
+// ---- Accessor Properties ----
+let book = {
+  year_: 2017,
+  edition: 1,
+};
+
+Object.defineProperty(book, "year", {
+  get() {
+    return this.year_;
+  },
+
+  set(newValue) {
+    if (newValue > 2017) {
+      this.year_ = newValue;
+      this.edition = this.edition + newValue - 2017;
+    }
+  },
+});
+
+console.log(book);
+book.year = 2019;
+console.log(book.edition);
